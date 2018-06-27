13bd1ac601000bf3505a0ef4de7e851aef6fc524
kylemazza <kyleamazza@gmail.com>
2018-06-27 16:44:12 -0700

    Added comments with a few instructions on how to locally test cloud functions

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -3,6 +3,18 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+// TESTING FUNCTIONS LOCALLY:
+// ### Testing HTTPS functions
+// https://firebase.google.com/docs/functions/local-emulator
+// -> This will just send you a link to a local server that will run your function for you.
+// ### Testing other functions
+// https://firebase.googleblog.com/2017/09/testing-functions-locally-with-cloud.html
+// -> Use the function shell
+//      ** Make sure you're in the firebase folder when your in your terminal!!!!
+//          --> /Quapp/src/firebase
+//      Run the command: "firebase experimental:functions:shell"
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
     console.log("HEYYYYY");
  response.send("Hello from Firebase!");
