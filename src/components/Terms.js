f7d8b7504fc322e44e0fce377091f53ef384ca24
kylemazza <kyleamazza@gmail.com>
2018-07-03 23:18:06 -0700

    Removed extraneous Card import

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
