68be2100da4b6b0bdfc6913c857cf0302231df02
kylemazza <kyleamazza@gmail.com>
2018-07-03 23:17:44 -0700

    Reformatted and added new font family declaration to logo text

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,57 +1,58 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import SchoolIcon from '@material-ui/icons/School';
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import SchoolIcon from "@material-ui/icons/School";
 
 const styles = theme => ({
     root: {
         paddingTop: theme.spacing.unit * 1,
         paddingBottom: theme.spacing.unit * 1,
         paddingLeft: theme.spacing.unit * 3,
-        paddingRight: theme.spacing.unit * 3,
+        paddingRight: theme.spacing.unit * 3
     },
     brand: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        flex: 1,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        flex: 1
     },
     icon: {
-        color: '#111',
+        color: "#111",
         width: 90,
         height: 90
     },
     title: {
         padding: 20,
         color: theme.palette.primary.contrastText,
-        '&:hover': {
-            textDecoration: 'underline'
-        }
+        "&:hover": {
+            textDecoration: "underline"
+        },
+        fontFamily: '"Monofett", "Roboto", "Helvetica", "Arial", sans-serif'
     },
     secondaryLink: {
         flex: 1,
         color: theme.palette.primary.contrastText,
-        '&:hover': {
-            textDecoration: 'underline'
+        "&:hover": {
+            textDecoration: "underline"
         }
     },
     flex: {
-        display: 'flex',
-        alignSelf: 'stretch'
+        display: "flex",
+        alignSelf: "stretch"
     },
     flex1: {
         flex: 1
     },
     contrastText: {
-        color: theme.palette.primary.contrastText,
+        color: theme.palette.primary.contrastText
     }
 });
 
@@ -68,23 +69,29 @@ class MenuBar extends React.Component {
             <AppBar className={classes.root} position="static">
                 <Toolbar>
                     <Link to="/" className={classes.brand}>
-                        <IconButton className={classes.icon} style={{}}>
-                            <SchoolIcon className={classes.icon}/>
+                        <IconButton className={classes.icon}>
+                            <SchoolIcon className={classes.icon} />
                         </IconButton>
-                        <Typography variant="display3" className={classes.title}>
+                        <Typography
+                            variant="display3"
+                            className={classes.title}>
                             Quapp!
                         </Typography>
                     </Link>
                     <Link to="/login" className={classes.flex}>
                         <Button className={classes.secondaryLink}>
-                            <Typography variant="title" className={classes.contrastText}>
+                            <Typography
+                                variant="title"
+                                className={classes.contrastText}>
                                 Log In
                             </Typography>
                         </Button>
                     </Link>
                     <Link to="/signup" className={classes.flex}>
                         <Button className={classes.secondaryLink}>
-                            <Typography variant="title" className={classes.contrastText}>
+                            <Typography
+                                variant="title"
+                                className={classes.contrastText}>
                                 Sign Up
                             </Typography>
                         </Button>
