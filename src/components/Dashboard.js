bfde365e10b71f2d0abc4c2c2b324bf13e00e3a9
kylemazza <kyleamazza@gmail.com>
2018-07-03 23:12:42 -0700

    Extracted QuizContainer component from Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,6 +11,7 @@ import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 
 import SideMenu from './SideMenu';
+import QuizContainer from './QuizContainer';
 
 const styles = theme => ({
     root: {
@@ -18,23 +19,6 @@ const styles = theme => ({
         marginTop: theme.spacing.unit * 7,
         border: '1px solid pink',
         paddingBottom: theme.spacing.paddingBottom
-    },
-    flexContainer: {
-
-    },
-    quizContainer: {
-        padding: '20px 0'
-    },
-    card: {
-        width: '100%'
-    },
-    quizCard: {
-        width: '100%',
-        height: 100,
-        maxHeight: 100
-    },
-    cardButton: {
-        width: '100%'
     }
 });
 
@@ -42,35 +26,18 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeQuizzes: [
-                {
-                    title: 'Quiz 5',
-                    class: 'CECS 341'
-                },
-                {
-                    title: 'Practice Quiz',
-                    class: 'CECS 1234'
-                },
-                {
-                    title: 'Open Book Exam 2',
-                    class: 'CECS 343'
-                },
-                {
-                    title: 'Closed Book Exam 2',
-                    class: 'CECS 343'
-                }
-            ],
-            pastQuizzes: [
-                {
-                    title: 'Open Book Exam 1',
-                    class: 'CECS 341'
-                }
-            ]
+            activeComponent: "ahhh"
         };
     }
+    handlePageChange = (event) => {
+        const component = event.component;
+        this.setState({
+            activeComponent: component
+        });
+    } 
     render() {
         const { classes } = this.props;
-        const { activeQuizzes, pastQuizzes } = this.state;
+        const { activeComponent } = this.state;
         return (
             <Grid 
                 className={classes.root} 
@@ -80,69 +47,10 @@ class Dashboard extends React.Component {
                 spacing={40}
             >
                 <Grid item xs={4} lg={3}>
-                    <SideMenu />
+                    <SideMenu handleChange={this.handlePageChange} />
                 </Grid>
                 <Grid item xs={8} lg={9}>
-                    <Grid className={classes.quizContainer} container direction="column">
-                        <Grid item>
-                            <Typography variant="headline">
-                                Active Quizzes
-                            </Typography>
-                            <Divider />
-                            <Grid item container>
-                                {
-                                    activeQuizzes.map((quiz, idx) => {
-                                        return (
-                                            <Grid item xs={4} key={idx}>
-                                                <Card className={classes.card}>
-                                                    <Button className={classes.cardButton} component={Link} to="/quiz">
-                                                        <CardContent className={classes.quizCard}>
-                                                            <Typography variant="title" component="h3">
-                                                                {quiz.title}
-                                                            </Typography>
-                                                            <Divider />
-                                                            <Typography variant="body1" component="p">
-                                                                {quiz.class}
-                                                            </Typography>
-                                                        </CardContent>
-                                                    </Button>
-                                                </Card>
-                                            </Grid>
-                                        );
-                                    })
-                                }
-                            </Grid>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="headline">
-                                Past Quizzes
-                            </Typography>
-                            <Divider />
-                            <Grid item container>
-                                {
-                                    pastQuizzes.map((quiz, idx) => {
-                                        return (
-                                            <Grid item xs={4} key={idx}>
-                                                <Card className={classes.card}>
-                                                    <Button className={classes.cardButton} component={Link} to="/quiz">
-                                                        <CardContent className={classes.quizCard}>
-                                                            <Typography variant="title" component="h3">
-                                                                {quiz.title}
-                                                            </Typography>
-                                                            <Divider />
-                                                            <Typography variant="body1" component="p">
-                                                                {quiz.class}
-                                                            </Typography>
-                                                        </CardContent>
-                                                    </Button>
-                                                </Card>
-                                            </Grid>
-                                        );
-                                    })
-                                }
-                            </Grid>
-                        </Grid>
-                    </Grid>
+                    {activeComponent}
                 </Grid>
             </Grid>
         );
