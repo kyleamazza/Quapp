a80a42bef5c0323ad01d4b2ff76e0e8a69877d9a
kylemazza <kyleamazza@gmail.com>
2018-07-03 23:12:23 -0700

    Reformatting via Prettier

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { hot } from 'react-hot-loader';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { indigo, pink } from '@material-ui/core/colors';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { hot } from "react-hot-loader";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { indigo, pink } from "@material-ui/core/colors";
 
-import MainRouter from './components/MainRouter';
+import MainRouter from "./components/MainRouter";
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            light: '#63a4ff',
-            main: '#1976d2',
-            dark: '#004ba0',
-            contrastText: '#fff',
+            light: "#63a4ff",
+            main: "#1976d2",
+            dark: "#004ba0",
+            contrastText: "#fff"
         },
         secondary: {
-            light: '#8e00f3',
-            main: '#5c6bc0',
-            dark: '#26418f',
-            contrastText: '#000',
+            light: "#8e00f3",
+            main: "#5c6bc0",
+            dark: "#26418f",
+            contrastText: "#000"
         },
-        openTitle: indigo['400'],
-        protectedTitle: pink['400'],
-        type: 'light'
+        openTitle: indigo["400"],
+        protectedTitle: pink["400"],
+        type: "light"
     },
     spacing: {
         unit: 8,
         paddingBottom: 280
+    },
+    typography: {
+        display3: {
+            color: "#000"
+        }
     }
 });
 
 const styles = {
     root: {
-        position: 'relative',
-        minHeight: '100%'
+        position: "relative",
+        minHeight: "100%"
     }
-}
+};
 
 class App extends React.Component {
     render() {
